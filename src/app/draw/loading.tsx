37f5af455c3b9262310f5ca745b3ef3b7631251f
micai <47612411+micai98@users.noreply.github.com>
2024-05-10 00:19:47 +0200

    remove an obsolete workaround

--- src/app/draw/loading.tsx
+++ src/app/draw/loading.tsx
@@ -1,8 +1,6 @@
 import Spinner from "@/components/ui/Spinner";
 
 const Loading = () => {
-    // WORKAROUND: loading pages for components that use localStorage will throw errors, this should prevent that
-    const localStorage:Storage | null = null;
     return <div className="centerscreen"><Spinner size="6rem"/></div>
 }
 
