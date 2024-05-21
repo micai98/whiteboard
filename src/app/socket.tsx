87e75b2856e4d845fba859aa980974147fe32f90
micai <47612411+micai98@users.noreply.github.com>
2024-05-21 21:51:52 +0200

    moved the backend URL to an env variable

--- src/app/socket.tsx
+++ src/app/socket.tsx
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client"
-const URL = "http://localhost:3001";
+const URL = process.env.NEXT_PUBLIC_BACKEND_ADDRESS || "http://localhost:3001";
 const socket = io(URL, { autoConnect: false, multiplex: false});
 export default socket;
